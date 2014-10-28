17158c609d894133d10bd3e704485c57747733cd
jianyaoang <jianyaoang1@gmail.com>
2014-10-28 16:56:11 +0800

    Created Harry Potter characters and populate them onto tableview

---
+++ Biodata-Swift/Biodata.swift
@@ -0,0 +1,20 @@
+//
+//  Biodata.swift
+//  Biodata-Swift
+//
+//  Created by Jian Yao Ang on 10/28/14.
+//  Copyright (c) 2014 Jian Yao Ang. All rights reserved.
+//
+
+import Foundation
+
+class Biodata: NSObject {
+    
+    var name: NSString?
+    var job: NSString?
+    var school: NSString?
+    var age: Int?
+    var magic: NSString?
+    
+    
+}
---
+++ Biodata-Swift/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  Biodata-Swift
+//
+//  Created by Jian Yao Ang on 10/28/14.
+//  Copyright (c) 2014 Jian Yao Ang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var biodataTableView: UITableView!
+    var biodata = NSArray()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        createBiodataList()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+
+    func createBiodataList()
+    {
+        var biodata1 = Biodata()
+        biodata1.name = "Albus"
+        biodata1.age = 12
+        biodata1.job = "Professor"
+        biodata1.school = "Hogwarts"
+        biodata1.magic = "Confundo"
+        
+        var biodata2 = Biodata()
+        biodata2.name = "Voldermort"
+        biodata2.age = 22
+        biodata2.job = "Master of Dark Arts"
+        biodata2.school = "Hogwarts"
+        biodata2.magic = "Avada Kedavra"
+        
+        var biodata3 = Biodata()
+        biodata3.name = "Harry"
+        biodata3.age = 19
+        biodata3.job = "Student"
+        biodata3.school = "Hogwarts"
+        biodata3.magic = "Expelliarmus"
+        
+        biodata = [biodata1, biodata2, biodata3]
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return biodata.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        var theBiodata = Biodata()
+        theBiodata = biodata[indexPath.row] as Biodata
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier("biodataCell") as UITableViewCell
+        
+        cell.textLabel.text = theBiodata.name
+        cell.detailTextLabel!.text = theBiodata.job
+        
+        return cell
+    }
+}
+
